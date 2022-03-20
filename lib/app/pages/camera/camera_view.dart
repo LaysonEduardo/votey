d33cdb987d413d38015233edcbe176a106956a6f
LaysonRockSoftware <layson@rocksoftware.com.br>
2022-03-20 02:38:03 -0300

    iOS Updates

--- lib/app/pages/camera/camera_view.dart
+++ lib/app/pages/camera/camera_view.dart
@@ -1,6 +1,5 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:votey/app/pages/camera/camera_controller.dart';
 import 'package:votey/app/utils/colors.dart';
