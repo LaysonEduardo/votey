7f4e92f73834534d6c6d5738639941bcfeaad452
LaysonEduardo <layson.contato@gmail.com>
2022-03-21 00:16:50 -0300

    login and logout updated to AppWrite

--- lib/data/repositories/appwrite_repo.dart
+++ lib/data/repositories/appwrite_repo.dart
@@ -1,8 +1,10 @@
 import 'package:appwrite/appwrite.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:votey/app/routes/app_routes.dart';
+import 'package:votey/app/widgets/snacksBar.dart';
 
-class AppWriteRepo extends GetxService {
+class AppWriteCli extends GetxService {
   Client client = Client();
   late Account account;
 
@@ -10,22 +12,47 @@ class AppWriteRepo extends GetxService {
   void onInit() {
     client.setEndpoint('https://192.168.1.66:443/v1').setProject('622e4d09dd1a4829385e').setSelfSigned(status: true);
     account = Account(client);
-    testRequest();
+    checkSession();
     super.onInit();
   }
 
-  testRequest() async {
-    await account.create(
-      userId: '1231',
-      email: 'me@appwrite.io',
-      password: 'password',
-      name: 'My Name',
-    );
+  logIn(String email, String password) async {
+    try {
+      await account.createSession(email: email, password: password).then((value) {
+        Get.offAllNamed(AppRoutes.home);
+      });
+    } on AppwriteException catch (e) {
+      print(e);
+      errorSnackBar(e.message ?? 'login error');
+    }
+  }
+
+  logOut() async {
+    try {
+      await account.deleteSession(sessionId: 'current').then((value) {
+        Get.offAllNamed(AppRoutes.onboarding);
+      });
+    } on AppwriteException catch (e) {
+      print(e);
+      errorSnackBar(e.message ?? 'logout error');
+    }
+  }
+
+  checkSession() async {
+    try {
+      await account.getSession(sessionId: 'current');
+      Get.offAllNamed(AppRoutes.home);
+    } on AppwriteException catch (e) {
+      print(e);
+      Get.offAllNamed(AppRoutes.onboarding);
+    }
   }
 }
 
 initAppWriteServices() async {
-  debugPrint('starting AppWrite Services...');
-  await Get.putAsync<AppWriteRepo>(() async => AppWriteRepo());
+  debugPrint(
+    'starting AppWrite Services...',
+  );
+  await Get.putAsync<AppWriteCli>(() async => AppWriteCli());
   debugPrint('All AppWrite services started');
 }
